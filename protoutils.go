a59b581d9882aa006ab367f1cd69aff4020a9edc
Steve Briskin <steve@viam.com>
2024-03-06 21:36:05 -0500

    remove dead consts

--- protoutils.go
+++ protoutils.go
@@ -13,17 +13,6 @@ import (
 	"go.viam.com/rdk/spatialmath"
 )
 
-const (
-	typeAngularVelocity          = "angular_velocity"
-	typeVector3                  = "vector3"
-	typeEuler                    = "euler"
-	typeQuat                     = "quat"
-	typeGeopoint                 = "geopoint"
-	typeOrientationVector        = "orientation_vector_radians"
-	typeOrientationVectorDegrees = "orientation_vector_degrees"
-	typeAxisAngle                = "r4aa"
-)
-
 func goToProtoCompatibleStruct(v interface{}) interface{} {
 	switch x := v.(type) {
 	case spatialmath.AngularVelocity:
